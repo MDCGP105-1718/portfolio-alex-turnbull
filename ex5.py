67fd1022db9a7bb19b9e22ea2dbf2ad042b9929b
CCrewe <S171298@UCS.ac.uk>
2017-10-05 09:51:22 +0100

    99 bottles of beer task

---
+++ ex5.py
@@ -0,0 +1,5 @@
+for i in range(99,0,-1):
+    if (i == 1):
+        print(str(i),"bottle of beer on the wall,",str(i),"bottle of beer!\nSo take it down, pass it around, no more bottles of beer on the wall!")
+    else:
+        print(str(i),"bottles of beer on the wall,",str(i),"bottles of beer.\nTake one down, pass it around,",str(i-1),"bottles of beer on the wall...")
